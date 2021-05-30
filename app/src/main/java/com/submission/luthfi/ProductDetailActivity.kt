79df33248a8b1c69141c54c65aa683a9b1f291d0
Luthfi Aji <luthfi.ajiw@gmail.com>
2021-05-30 19:35:29 +0700

    product details & profile view

--- app/src/main/java/com/submission/luthfi/ProductDetailActivity.kt
+++ app/src/main/java/com/submission/luthfi/ProductDetailActivity.kt
@@ -1,10 +1,21 @@
 package com.submission.luthfi
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.submission.luthfi.R.color.titleColor
+import org.w3c.dom.Text
 
 class ProductDetailActivity : AppCompatActivity() {
+    private lateinit var imgPhoto: ImageView
+    private lateinit var name: TextView
+    private lateinit var price: TextView
+    private lateinit var detail: TextView
+
     companion object {
         const val EXTRA_PRODUCT = "product"
     }
@@ -12,10 +23,19 @@ class ProductDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val product: Product = intent.getSerializableExtra(EXTRA_PRODUCT) as Product
 
+        imgPhoto = findViewById(R.id.product_img)
+        name = findViewById(R.id.product_name)
+        price = findViewById(R.id.product_price)
+        detail = findViewById(R.id.product_detail)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        imgPhoto.setImageResource(product.img)
+        name.text = product.name
+        price.text = product.price
+        detail.setText(product.desc)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
